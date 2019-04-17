1f70fd1c9dc19364f1517f86c01571a234562f7d
jonathan nshuti <nshutijonathan130@gmail.com>
2019-04-17 16:35:40 +0200

    [ch-set-db]database configured.

--- server/database/db.js
+++ server/database/db.js
@@ -12,6 +12,31 @@ export const createTables=()=>{
 	isadmin VARCHAR(350) NOT NULL
 	)`;
 	const queries=`${users}`;
-    pool.query(queries);
+    pool.query(queries)
+    .then((res)=>{
+    	console.log(res);
+    	pool.end();
+    })
+    .catch((err)=>{
+    	console.log(err);
+    	pool.end();
+    });
+};
+export const dropTables=()=>{
+	const users='DROP TABLE IF EXISTS users';
+	pool.query(users)
+	.then((res)=>{
+		console.log(res);
+		pool.end();
+	})
+	.catch((err)=>{
+		console.log(err);
+		pool.end();
+	});
+	pool.on('remove',()=>{
+		console.log('client removed');
+		process.exit(0);
+
+	});
 };
 require('make-runnable');
